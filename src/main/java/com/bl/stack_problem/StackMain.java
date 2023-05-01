951901ae2d32f1fff61c0e95b2db9b0c3db982b2
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-01 08:12:14 +0530

    code added to UC-3 branch

--- src/main/java/com/bl/stack_problem/StackMain.java
+++ src/main/java/com/bl/stack_problem/StackMain.java
@@ -1,47 +1,30 @@
 package com.bl.stack_problem;
 import java.util.LinkedList;
 public class StackMain {
-    private LinkedList<Integer> stackList;
+    private LinkedList<Integer> queue;
 
     public StackMain() {
-        stackList = new LinkedList<Integer>();
+        queue = new LinkedList<Integer>();
     }
 
-    public void push(int item) {
-        stackList.addFirst(item);
+    public void enqueue(int value) {
+        queue.addLast(value);
     }
 
-    public int pop() {
-        return stackList.removeFirst();
-    }
-
-    public int peek() {
-        return stackList.getFirst();
-    }
-
-    public boolean isEmpty() {
-        return stackList.isEmpty();
-    }
-
-    public int size() {
-        return stackList.size();
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to Stack with linked list problem.");
-        StackMain stack = new StackMain();
-
-        stack.push(70);
-        stack.push(30);
-        stack.push(56);
-
-        System.out.println("Top element of the stack: " + stack.peek());
-
-        System.out.println("Elements popped from the stack:");
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop());
+    public void printQueue() {
+        for (int item : queue) {
+            System.out.print(item + " ");
         }
+    }
 
-        System.out.println("Size of the stack: " + stack.size());
+    public static void main(String[] args) throws IllegalStateException {
+        System.out.println("Welcome to Stack with linked list problem.");
+        StackMain queue = new StackMain();
+
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+
+        queue.printQueue();
     }
 }
